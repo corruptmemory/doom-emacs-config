3f4aae6a929f5b10514df253373ad80fb1283b5b
Jim Powers <jim@corruptmemory.com>
2019-12-10 12:14:59 -0500

    Fixes and stuff

--- config.el
+++ config.el
@@ -1,6 +1,8 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
+(setenv "PATH" (concat (getenv "PATH") ":/home/jim/go/bin"))
+(setq exec-path (append exec-path '("/home/jim/go/bin")))
 
 (defun my-doom-modeline--font-height ()
   "Calculate the actual char height of the mode-line."
@@ -10,6 +12,11 @@
 
 (setq
  display-line-numbers-type 'relative
- doom-font (font-spec :family "IBM Plex Mono" :size 32)
+ doom-font (font-spec :family "IBM Plex Mono" :size 16)
  projectile-project-search-path '("~/projects/" "~/moat/"))
 
+;;; Add to ~/.doom.d/config.el
+(setq doom-font (font-spec :family "IBM Plex Mono" :size 32 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "Fira Sans") ; inherits `doom-font''s :size
+      doom-unicode-font (font-spec :family "Input Mono Narrow" :size 12)
+      doom-big-font (font-spec :family "Fira Mono" :size 38))
